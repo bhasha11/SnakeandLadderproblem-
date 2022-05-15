2c11a52380f30fe0868f7f782ac167dbb24e75c1
anjali <anjalimalviya2532@gmail.com>
2022-05-15 17:27:13 +0530

    [Anjali]Add: Snake And Ladder Roll The Dice

---
+++ RollsTheDie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackLadderP
+{
+    public class RollsTheDie
+    {
+
+            public int StartPoint = 0; 
+            public int RanDice;
+            public void StartPlaying()
+            {
+                Console.WriteLine("Game Started! Good Luck "); 
+                Random Random = new Random(); 
+                RanDice = Random.Next(1, 7); 
+                Console.WriteLine("player Role Dice number is:" + " " + RanDice); 
+            }
+        }
+    }
+
+
